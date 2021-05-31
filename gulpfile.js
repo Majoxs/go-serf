ccfebbff4abba4d915831d7f197d4aa4d647ae92
Majoxs <majoxs@gmail.com>
2021-05-31 13:05:15 +0500

    added header page

--- gulpfile.js
+++ gulpfile.js
@@ -41,6 +41,7 @@ function images() {
 function scripts() {
    return src([
       'node_modules/jquery/dist/jquery.js',
+      'node_modules/slick-carousel/slick/slick.js',
       'app/js/main.js'
    ])
       .pipe(concat('main.min.js'))
@@ -51,7 +52,12 @@ function scripts() {
 
 
 function styles() {
-   return src('app/scss/style.scss')
+   return src([
+      'node_modules/normalize.css/normalize.css',
+      'node_modules/slick-carousel/slick/slick.css',
+      'node_modules/animate.css/animate.css',
+      'app/scss/style.scss'
+   ])
       .pipe(scss({ outputStyle: 'compressed' }))
       .pipe(concat('style.min.css'))
       .pipe(autoprefixer({
